3b086fdcb4df30ff0c2457fa4e6d4755f033a19e
Antoine Petit <antooine.petit@gmail.com>
2025-07-15 16:23:10 +0200

    feat: add user registration functionality
    - Created signup form (signup.php)
    - Implemented signUp function (functions.php)
    - Handles user input and database insertion
    - Redirects to index.php on success
    - Displays error message on failure

--- signup.php
+++ signup.php
@@ -1,2 +1,38 @@
 <?php 
-echo $_POST['id'];
+require_once('functions.php');
+if(!empty($_GET['mode']) && $_GET['mode'] == 'register'){
+  echo "Test";
+  if(!empty($_POST['username']) && !empty($_POST['email'])){
+    $retour = signUp($pdo, $_POST['username'], $_POST['email']) ;
+    if($retour > 0){
+      header('Location: index.php');
+    } else {
+      echo 'Erreur survenue';
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Créer un compte</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <form action="signup.php?mode=register" method="post">
+    <div>
+      <label for="username">Nom d'utilisateur</label>
+      <input type="text" name="username" id="username">
+    </div>
+
+    <div>
+      <label for="email">Email :</label>
+      <input type="email" name="email" id="email">
+    </div>
+
+    <input type="submit" value="S'inscrire et retourner à l'accueil">
+  </form>
+</body>
+</html>
