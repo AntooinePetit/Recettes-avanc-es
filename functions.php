18917ee149a9befc0edb6d32cfdc5f38669c1cf4
Antoine Petit <antooine.petit@gmail.com>
2025-07-15 11:23:24 +0200

    feat: functions & tag display
    - Created a function to get recipes' tags
    - Added tags display on page
    - Added comments to explain functions

--- functions.php
+++ functions.php
@@ -2,12 +2,14 @@
 
 require_once('db.php');
 
+// Fonction pour récupérer toutes les recettes
 function getRecipes($pdo){
   $stmt = $pdo->prepare('SELECT r.id_recette, r.nom_recette, r.temps_preparation_min, r.temps_cuisson_min, r.difficulte, c.nom_categorie FROM recettes as r INNER JOIN categories_recettes as c ON r.fk_id_categorie = c.id_categorie');
   $stmt->execute();
   return $stmt->fetchAll();
 }
 
+// Fonction pour mettre à jour la difficulté des recettes
 function updateDifficulty($pdo, $tempsTotal, $difficulte, $id){
   if($tempsTotal <= 180){
     $nouvelleDifficulte = 'Facile';
@@ -27,4 +29,11 @@ function updateDifficulty($pdo, $tempsTotal, $difficulte, $id){
     return $nouvelleDifficulte;
   }
   return $difficulte;
+}
+
+// Fonction pour récupérer les tags d'une recette
+function getTags($pdo, $id){
+  $stmt = $pdo->prepare('SELECT t.nom_tag FROM recette_tag as r INNER JOIN tags_recettes as t ON r.fk_id_tag = t.id_tag WHERE r.fk_id_recette = :id');
+  $stmt->execute(["id" => $id]);
+  return $stmt->fetchAll();
 }
--- index.php
+++ index.php
@@ -33,11 +33,21 @@ require_once('functions.php');
       foreach($recettes as $recette):
         $tempsTotal = $recette['temps_preparation_min'] + $recette['temps_cuisson_min'];
         $difficulte = updateDifficulty($pdo, $tempsTotal, $recette['difficulte'], $recette['id_recette']);
+        $tags = getTags($pdo, $recette['id_recette']);
+        $last_key = array_key_last($tags);
+        $i = 0;
+        // var_dump($tags)
       ?>
         <tr>
           <td><?= $recette['nom_recette'] ?></td>
           <td><?= $recette['nom_categorie'] ?></td>
-          <td></td>
+          <td><?php foreach($tags as $tag){
+            echo $tag['nom_tag'];
+            if($i != $last_key) {
+              echo ', ';
+            }
+            $i++;
+          } ?></td>
           <td><?= number_format($tempsTotal/60) ?>H<?php if($tempsTotal%60 != 0){echo $tempsTotal%60;} ?></td>
           <td><?= $difficulte ?></td>
           <td><a href="recette.php?id=<?= $recette['id_recette'] ?>">Voir la recette</a></td>
